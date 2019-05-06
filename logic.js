f69e3e540dbcc703372db5cc4bebd3140edc5fe6
tbiaglow <tbiaglow@gmail.com>
2019-05-05 23:07:25 -0400

    fixed randomWord function, created isCorrectGuess function

--- logic.js
+++ logic.js
@@ -1,10 +1,20 @@
 var gameWords = ["Miguel Diaz", "Johnny Lawrence", "Hawk", "Daniel Larusso", "Samantha Larusso"];
 
-function randomWord(x) {
-    rand = math.random
-    for (i = 1; i<gameWords.length; i++) {
+function randomWord(gameWords) {
+    var rand = Math.random();
+    for (i = 1; i<=gameWords.length; i++) {
         if (i / gameWords.length >= rand) {
             return gameWords[i - 1]
         }
     }
 }
+
+function isCorrectGuess(word, letter) {
+    if (word.includes(letter) === true) {
+        return true
+    }
+    else if (word.includes(letter) === false) {
+        return false
+    }
+}
+
