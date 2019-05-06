735991c38a855c7ead18cb60bd33667ff4e13535
tbiaglow <tbiaglow@gmail.com>
2019-05-05 22:29:15 -0400

    Fixed gameWords array, created randomWord function

--- logic.js
+++ logic.js
@@ -1,2 +1,10 @@
-var gameWords = [Miguel Diaz, Johnny Lawrence, Hawk, Daniel Larusso, Samantha Larusso];
+var gameWords = ["Miguel Diaz", "Johnny Lawrence", "Hawk", "Daniel Larusso", "Samantha Larusso"];
 
+function randomWord(x) {
+    rand = math.random
+    for (i = 1; i<gameWords.length; i++) {
+        if (i / gameWords.length >= rand) {
+            return gameWords[i - 1]
+        }
+    }
+}
